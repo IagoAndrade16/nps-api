457c789cbed20a18a5ad9ab01c9ba9b301067ae0
Iago Alexandre <iagoaap@outlook.com>
2023-04-07 10:25:44 -0300

    list all surveys method

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -6,12 +6,20 @@ export class SurveysController {
   async create(req: Request, res: Response): Promise<Response> {
     const { title, description } = req.body;
 
-    const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveysRepository = getCustomRepository(SurveysRepository)
 
     const survey = surveysRepository.create({ title, description })
 
     await surveysRepository.save(survey)
-    
+
     return res.status(201).json(survey);
+  }
+
+  async show(req: Request, res: Response): Promise<Response> {
+    const surveysRepository = getCustomRepository(SurveysRepository)
+
+    const listOfAllSurveys = await surveysRepository.find();
+
+    return res.json(listOfAllSurveys)
   }
 }
