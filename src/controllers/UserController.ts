8096b3b45ecabddee630b9c4d1ae4e36408caf41
Iago Alexandre <iagoaap@outlook.com>
2023-04-07 10:07:06 -0300

    create users repository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 export class UserController {
   async create(req: Request, res: Response): Promise<Response> {
     const { name, email } = req.body
 
-    const usersRepository = getRepository(User)
+    const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({
       where: {
